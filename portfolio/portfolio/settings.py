1cd1dd138b0f661407edb1d7c6fc818f0f7771e3
petyr2 <pmm00225@gmail.com>
2025-09-12 11:22:15 +0300

    Update portfolio

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"    #This is the directory where we're going to store different media files
+MEDIA_ROOT = os.path.join(BASE_DIR, "mdeia") #where the media folder is actually stored
