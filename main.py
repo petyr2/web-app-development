0d95212657ebfc174c1825f91ac75170bd1924fb
petyr2 <pmm00225@gmail.com>
2025-09-14 05:51:33 +0300

    Update on Sun Sep 14 05:51:33 EAT 2025

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/bio")
+def page():
+	return render_template("bio.html")
+
+@app.route("/my portfolio")
+def pro():
+	return render_template("projects.html")
+
+@app.route("/pitch")
+def pit():
+	return render_template("pitch.html")
+
+if __name__ =="__main__":
+
+	app.run()
